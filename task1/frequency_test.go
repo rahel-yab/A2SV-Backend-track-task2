46fc394a7cbf06ead27d33f40831743a48e6c876
rahel yabebal <rahelyabebal@gmail.com>
2025-07-12 23:47:28 +0300

    improved frequency function by cleaning input

--- task1/frequency_test.go
+++ task1/frequency_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestFrequency(t *testing.T) {
-    input := "go go gone"
+    input := "go go gone!!!.."
     expected := map[string]int{"go": 2, "gone": 1}
 
     result := frequency(input)
--- task1/main.go
+++ task1/main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
+func cleanInput(input string) string {
+    var curr strings.Builder
+    for _, r := range input {
+        if unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsSpace(r) {
+            curr.WriteRune(unicode.ToLower(r))
+        }
+    }
+    return curr.String()
+}
+
 func frequency(input string) map[string]int {
-	dict := make(map[string]int)
-	words := strings.Fields(input)
-	for _, word := range words {
-		dict[word] ++
-	}
-	return dict
+    dict := make(map[string]int)
+    cleaned := cleanInput(input)
+    words := strings.Fields(cleaned)
+    for _, word := range words {
+        dict[word]++
+    }
+    return dict
 }
 
 func main() {
-	fmt.Println(frequency("I am learning go"))
+    fmt.Println(frequency("I am learning Go. go GO!"))
 }
